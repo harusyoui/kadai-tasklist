81e67c91e159d58233453cbaee157bc9d8fdd070
harutsugu.nakamura <harusyoui@docomo.ne.jp>
2018-08-06 05:23:27 +0000

    kadai L12-1-2

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 Route::get('/',function(){
-    return view('welcome');
+    return view('tasks');
 });
 Route::get('tasks','TasksController@index');
 Route::resource('tasks','TasksController');
